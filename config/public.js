815ec0fed894b4129e0a2b07363b200ce7604a1a
huangwutao <346792184@qq.com>
2019-01-28 16:25:21 +0800

    修复打包功能

--- config/public.js
+++ config/public.js
@@ -1,5 +1,6 @@
 const { isDev, type } = require('../lib/args')
 const config = require('../lib/config')
+const resolve = require('../lib/resolve')
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
@@ -10,14 +11,13 @@ module.exports = {
     compress: true,    // 服务端压缩是否开启
     port: config.dev.port, // 端口
     hot: true, //热替换
-    noInfo: true,
-    stats: 'errors-only',
+    noInfo: false,
     overlay: { //页面弹出错误信息
       warnings: false,
       errors: true
     },
     progress: true, //输出进度到控制台
-    quiet: true,
+    quiet: false,
     openPage: config.dev.open,
   },
   externals: config.default.externals,
