0b9a2b346d480bff710d079d6329293674a66b97
huangwutao <346792184@qq.com>
2019-11-04 17:38:33 +0800

    新增命令行传参

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const rl = require('./lib/rl')
 const env = require('./lib/env')
 const { packages } = require('./lib/fs.js')
+const config = require('./lib/config')
 
 global._G = {}
 
-env().then((env) => {
-  _G.env = env
-  rl(packages).then(function (md) {
-    _G.packages = md
-    require('./bin/index')()
-  })
+var args = process.argv
+var argEnv = args.filter((e) => {
+  return e in config.default.env
 })
+var argPackage = args.filter((e) => {
+  return packages.indexOf(e) >= 0
+})
+
+if (argEnv.length > 0) {
+  _G.env = argEnv[0]
+  _G.packages = argPackage
+  require('./bin/index')()
+} else {
+  env().then((env) => {
+    _G.env = env
+    rl(packages).then(function (md) {
+      _G.packages = md
+      require('./bin/index')()
+    })
+  })
+}
+
