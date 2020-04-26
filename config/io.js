e0775f62c456665a7916b67cfac142ba63001ecd
黄武韬 <346792184@qq.com>
2020-04-26 18:21:37 +0800

    新增output可配置

--- config/io.js
+++ config/io.js
@@ -1,13 +1,31 @@
 const entrys = require('../lib/ps')
 const resolve = require('../lib/resolve')
 const config = require('../lib/config')
-
-var io = {
-  entry: entrys,
-  output: {
+const {__isEmptyObject} = require('../lib/help')
+var output
+if (__isEmptyObject(config.default.output)) {
+  output = {
+    path: resolve('dist'),
+    filename: `[name].js`,
+    publicPath: '../../'
+  }
+} else if (typeof config.default.output == 'string') {
+  output = {
     path: resolve(config.default.output),
     filename: `[name].js`,
     publicPath: '../../'
   }
+} else if (typeof config.default.output == 'object') {
+  output = {
+    path: resolve('dist'),
+    filename: `[name].js`,
+    publicPath: '../../',
+    ...config.default.output
+  }
+}
+
+var io = {
+  entry: entrys,
+  output
 }
 module.exports = io
