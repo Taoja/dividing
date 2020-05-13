9fefc5916d806975e95b00ca640e145fd8b09c2d
黄武韬 <346792184@qq.com>
2020-05-13 14:49:32 +0800

    新增output可配置

--- bin/index.js
+++ bin/index.js
@@ -5,12 +5,22 @@ const config = require('../lib/config')
 const resolve = require('../lib/resolve')
 const options = require('../config/options')
 const { isDev } = require('../lib/args')
+const {__isEmptyObject} = require('../lib/help')
+
+var output
+if (__isEmptyObject(config.default.output)) {
+  output = resolve('dist')
+} else if (typeof config.default.output == 'string') {
+  output = resolve(config.default.output)
+} else if (typeof config.default.output == 'object') {
+  output = config.default.output.path
+}
 
 const rm = function () {
   if (isDev) {
     options()
   } else {
-    rmrf(resolve(config.default.output), function() {
+    rmrf(resolve(output), function() {
       options()
     })
   }
